727ad5495edb1d1358bd7f8897099dba332fe7ab
Paco Chacon <pacochacon94@gmail.com>
2018-02-10 18:50:04 -0600

    Added LoginViewController to project

---
+++ PowerHome/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  PowerHome
+//
+//  Created by Francisco Javier Chacon de Dios on 10/02/18.
+//  Copyright © 2018 Francisco Javier Chacon de Dios. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
