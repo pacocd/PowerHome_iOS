7aca7f899cc1c289fbcd682394b12e4103c0a6fe
Paco Chacon <pacochacon94@gmail.com>
2018-02-10 20:27:10 -0600

    Added Colors and LoginView to project

---
+++ PowerHome/Colors.swift
@@ -0,0 +1,9 @@
+//
+//  Colors.swift
+//  PowerHome
+//
+//  Created by Francisco Javier Chacon de Dios on 10/02/18.
+//  Copyright © 2018 Francisco Javier Chacon de Dios. All rights reserved.
+//
+
+import Foundation
---
+++ PowerHome/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  PowerHome
+//
+//  Created by Francisco Javier Chacon de Dios on 10/02/18.
+//  Copyright © 2018 Francisco Javier Chacon de Dios. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
