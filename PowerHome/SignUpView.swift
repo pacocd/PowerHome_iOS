380ebcbacabce90746298e0cbda73d7006c90036
Paco Chacon <pacochacon94@gmail.com>
2018-02-11 02:10:08 -0600

    Added Sign up view and controller, also test files

---
+++ PowerHome/SignUpView.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpView.swift
+//  PowerHome
+//
+//  Created by Francisco Javier Chacon de Dios on 11/02/18.
+//  Copyright © 2018 Francisco Javier Chacon de Dios. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
