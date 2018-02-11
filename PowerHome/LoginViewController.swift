f23c131a4f5040ae6f5f3aac4f21efd8a5ed08aa
Paco Chacon <pacochacon94@gmail.com>
2018-02-11 00:40:06 -0600

    Set LoginView as LoginViewController view

--- PowerHome/LoginViewController.swift
+++ PowerHome/LoginViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let loginView: LoginView = LoginView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+
+        view.addSubview(loginView)
         // Do any additional setup after loading the view.
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        let loginViewHorizontalConstraints: [NSLayoutConstraint] = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[loginView]-|", options: .alignAllCenterX, metrics: nil, views: ["loginView": loginView])
+        let loginViewVerticalConstraints: [NSLayoutConstraint] = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[loginView]-|", options: .alignAllCenterY, metrics: nil, views: ["loginView": loginView])
+
+        view.addConstraints(loginViewVerticalConstraints)
+        view.addConstraints(loginViewHorizontalConstraints)
     }
 
 }
